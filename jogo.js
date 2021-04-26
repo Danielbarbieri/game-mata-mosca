e1629829a2ff6b50c4531bf6bafe2796c6f4258f
Daniel <danielbarbierianalista@gmail.com>
2021-04-26 14:47:30 -0300

    continuação2604

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -15,6 +16,15 @@ function posicaoRandomica() {
 
 	if (document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
+
+		if (vidas > 3) {
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+			vidas++
+		}
+
 	}
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
